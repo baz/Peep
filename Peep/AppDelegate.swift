025f57ec990e3839b3b208372ca64d0f9ffb6249
Basil Shkara <basil@neat.io>
2017-09-15 13:16:12 +0200

    Initial Commit

---
+++ Peep/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Peep
+//
+//  Created by Basil Shkara on 15/9/17.
+//  Copyright © 2017 Neat.io. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
